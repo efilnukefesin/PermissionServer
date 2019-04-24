5e7419b77eb2ea4a84f898486604aee77ff2841b
efilnukefesin <efil.threat@googlemail.com>
2019-04-24 21:51:56 +0200

    initial commit containing simple project structure, build pipeline set up roughly.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # PermissionServer
-Demo implementation of a Permission system using IdentityServer4
+Demo implementation of a Permission system using IdentityServer4.
+
+This repository aims to show possible approaches to the following scenarios.
+You might/should/must want to:
+- Identify a User
+- check the permissions of this user in a WPF project
+- manage permissions of this user in a WPF project
+- let this user try to get new permissions
+- let the permission owner decide whether to grant these
+- let the permission owner have an overview of users using his permissions
+- ...
+
+This solution follows these principles:
+- seperate Identity from Permissions
+- KISS (hopefully ;-))
+- leverage .NET Standard
+- provide real life examples of above scenarios
+- MVVM (I'm trying not to overengineer [VMs for UserControls, seperated Projects in all detail, ...])
+- ...
+
+So let's introduce the members of the play:
+- Bob (he's the user)
+- Nigel (he's the boss/permission owner)
+- Dom (he's the admin, a particular nice one)
+
+We probably need some pieces of software, too:
+
+- a client app with the MegaImportantFunction (tm)
+- an IdentityServer4 implementation
+- a Permission dealing Permission Server
+- an admin app
---
+++ build/Builder/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using McMaster.Extensions.CommandLineUtils;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+namespace Builder
+{
+    class Program
+    {
+        #region Properties
+
+        private const bool areTestsRequired = true;
+
+        private const string build = "build";
+        private const string test = "test";
+        private const string pack = "pack";
+        private const string @default = "default";
+
+        //private const string slnPath = "../../../../../";
+        //private const string srcPath = "../../../../../src";
+        //private const string testPath = "../../../../../test";  //original: "./test"
+        //private const string artifactsPath = "../../../../artifacts";
+        private const string slnPath = ".";
+        private const string srcPath = "./src";
+        private const string testPath = "./test";  //original: "./test"
+        private const string artifactsPath = "./artifacts";
+
+        #endregion Properties
+
+        #region Methods
+
+        #region Main
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication(throwOnUnexpectedArg: false);
+
+            cleanArtifacts();
+
+            app.OnExecute(() =>
+            {
+                Target(build, () =>
+                {
+                    System.Console.WriteLine($"{build}: Looking for SLN...");
+                    //var s = Path.GetFullPath(SrcPath);
+                    var solution = Directory.GetFiles(slnPath, "*.sln", SearchOption.TopDirectoryOnly).First();
+
+                    Run("dotnet", $"build {solution} -c Release");
+                });
+                Target(test, DependsOn(build), () =>
+                {
+                    System.Console.WriteLine($"{test}: Looking for Tests...");
+                    try
+                    {
+                        var s = Path.GetFullPath(testPath);
+                        var tests = Directory.GetFiles(testPath, "*.csproj", SearchOption.AllDirectories);
+
+                        foreach (var test in tests)
+                        {
+                            System.Console.WriteLine($"{Program.test}: Adding Test '{test}'");
+                            Run("dotnet", $"test {test} -c Release --no-build");
+                        }
+                    }
+                    catch (System.IO.DirectoryNotFoundException ex)
+                    {
+                        if (areTestsRequired)
+                        {
+                            throw new Exception($"{test}: No tests found: {ex.Message}");
+                        };
+                    }
+                    System.Console.WriteLine($"{test}: Stopped looking for Tests");
+                });
+                Target(pack, DependsOn(test), () =>
+                {
+                    var projects = Directory.GetFiles(srcPath, "*.csproj", SearchOption.AllDirectories);
+
+                    foreach (var project in projects)
+                    {
+                        System.Console.WriteLine($"{pack}: Adding Project '{project}'");
+                        Run("dotnet", $"pack {project} -c Release -o {artifactsPath} --no-build");
+                    }
+                });
+                Target(@default, DependsOn(test, pack));
+                RunTargetsAndExit(app.RemainingArguments);
+            });
+
+            app.Execute(args);
+        }
+        #endregion Main
+
+        #region cleanArtifacts
+        private static void cleanArtifacts()
+        {
+            Directory.CreateDirectory($"{artifactsPath}");
+
+            foreach (var file in Directory.GetFiles($"{artifactsPath}"))
+            {
+                File.Delete(file);
+            }
+        }
+        #endregion cleanArtifacts
+
+        #endregion Methods
+    }
+}
