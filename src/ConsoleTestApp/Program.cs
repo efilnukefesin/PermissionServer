0ee48185bdb5bdfcc58a8669adae82e8b07fce14
efilnukefesin <efil.threat@googlemail.com>
2019-04-25 22:07:16 +0200

    started implementing ConsoleTestApp and some structuring of the solution

--- src/ConsoleTestApp/Program.cs
+++ src/ConsoleTestApp/Program.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleTestApp
 {
     class Program
     {
+        #region Properties
+
+        private static Dictionary<ConsoleKeyInfo, SimpleMenuItem> menuEntries;
+        private static bool isQuitting = false;
+
+        #endregion Properties
+
+        #region Methods
+
+        #region Main
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            initMenu();
+
+            while (!isQuitting)
+            {
+                renderMenu();
+                ConsoleKeyInfo choice = Console.ReadKey(true);
+                if (menuEntries.Keys.Any(x => x.Equals(choice)))
+                {
+                    menuEntries[choice].Action.Invoke();
+                }
+            }
         }
+        #endregion Main
+
+        #region initMenu
+        private static void initMenu()
+        {
+            menuEntries = new Dictionary<ConsoleKeyInfo, SimpleMenuItem>();
+            menuEntries.Add(new ConsoleKeyInfo('1', ConsoleKey.D1, false, false, false), new SimpleMenuItem("Get me an identity!", () => { requestIdentity(); }));
+            menuEntries.Add(new ConsoleKeyInfo('q', ConsoleKey.Q, false, false, false), new SimpleMenuItem("Quit", () => { isQuitting = true; Console.WriteLine("Bye!"); }));
+        }
+        #endregion initMenu
+
+        #region renderMenuEntry
+        private static void renderMenuEntry(ConsoleKeyInfo keyInfo, SimpleMenuItem item)
+        {
+            Console.WriteLine($"{keyInfo.KeyChar}) {item.Caption}");
+        }
+        #endregion renderMenuEntry
+
+        #region renderMenu
+        private static void renderMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("MENU");
+            Console.WriteLine("====");
+            Console.WriteLine("");
+            foreach (KeyValuePair<ConsoleKeyInfo, SimpleMenuItem> kvp in menuEntries)
+            {
+                renderMenuEntry(kvp.Key, kvp.Value);
+            }
+            Console.WriteLine("\nChoose wisely.");
+        }
+        #endregion renderMenu
+
+        #region requestIdentity
+        private static void requestIdentity()
+        {
+
+        }
+        #endregion requestIdentity
+
+        #endregion Methods
     }
 }
---
+++ src/Models/SimpleMenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class SimpleMenuItem
+    {
+        #region Properties
+
+        public string Caption { get; set; }
+        public Action Action { get; set; }
+
+        #endregion Properties
+
+        #region Construction
+
+        public SimpleMenuItem(string Caption, Action Action)
+        {
+            this.Caption = Caption;
+            this.Action = Action;
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
