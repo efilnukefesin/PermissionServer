caf3b7aaad9c83e051ff8be81b6663abe2aafe20
efilnukefesin <efil.threat@googlemail.com>
2019-04-25 22:32:05 +0200

    added some useful comments

--- src/BootStrapper/DiHelper.cs
+++ src/BootStrapper/DiHelper.cs
@@ -5,11 +5,19 @@ using System.Text;
 
 namespace BootStrapper
 {
+    /// <summary>
+    /// helper class to avoid direct calls to DiManager class
+    /// </summary>
     public static class DiHelper
     {
         #region Methods
 
-        #region GetService
+        #region GetService: gets a service implementation from the DI Container
+        /// <summary>
+        /// gets a service implementation from the DI Container
+        /// </summary>
+        /// <typeparam name="I">the interface you need the implementation for</typeparam>
+        /// <returns>the service implementation</returns>
         public static I GetService<I>()
         {
             return DiManager.GetInstance().Resolve<I>();
