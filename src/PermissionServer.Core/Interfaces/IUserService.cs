f93e517b3af292a9b35f1a9ce4ed8b455a36f835
efilnukefesin <efil.threat@googlemail.com>
2019-04-28 22:10:42 +0200

    added UserService

---
+++ src/PermissionServer.Core/Interfaces/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using NET.efilnukefesin.Contracts.Base;
+using PermissionServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionServer.Core.Interfaces
+{
+    public interface IUserService : IBaseObject
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        void CreateTestUsers();
+        User GetUserBySubject(string SubjectId);
+
+        #endregion Methods
+    }
+}
---
+++ src/PermissionServer.Core/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using NET.efilnukefesin.Implementations.Base;
+using PermissionServer.Core.Interfaces;
+using PermissionServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionServer.Core.Services
+{
+    public class UserService : BaseObject, IUserService
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        #endregion Construction
+
+        #region Methods
+
+        #region CreateTestUsers
+        public void CreateTestUsers()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion CreateTestUsers
+
+        #region GetUserBySubject
+        public User GetUserBySubject(string SubjectId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion GetUserBySubject
+
+        #region dispose
+        protected override void dispose()
+        {
+            //TODO: implement
+        }
+        #endregion dispose
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+
+    }
+}
