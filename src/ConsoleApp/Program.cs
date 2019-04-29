66133ac4b12d2d2d378e5daab0a7b45fcaf86a79
efilnukefesin <efil.threat@googlemail.com>
2019-04-29 22:19:26 +0200

    as I am running into a stack overflow exxception with the net sdk 3 preview 4, I added a nicer version of the console app - first iteration.

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SadConsole;
+using Microsoft.Xna.Framework;
+using Console = SadConsole.Console;
+using ConsoleApp.Consoles;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        private static MenuConsole menuConsole;
+
+        [STAThread]
+        static void Main()
+        {
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(80, 25);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        static void Init()
+        {
+            // Any setup
+            if (Settings.UnlimitedFPS)
+                SadConsole.Game.Instance.Components.Add(new SadConsole.Game.FPSCounterComponent(SadConsole.Game.Instance));
+
+            SadConsole.Game.Instance.Window.Title = "PermissionServer Demo Client";
+
+            menuConsole = new MenuConsole();
+            Global.CurrentScreen = menuConsole;
+        }
+    }
+}
--- src/PermissionServer/Controllers/PermissionsController.cs
+++ src/PermissionServer/Controllers/PermissionsController.cs
@@ -43,7 +43,7 @@ namespace PermissionServer.Controllers
                 //TODO: extract Subject Id from token and pass to permissionService
                 string subjectId = "88421113";
                 User user = permissionService.GetUser(subjectId);
-                result = new SimpleResult<object>(user);
+                result = new SimpleResult<object>(user); 
             }
             else
             {
