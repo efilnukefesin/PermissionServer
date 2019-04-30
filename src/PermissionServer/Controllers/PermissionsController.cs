e5fa813921f5ad4883127a674e2b045c77e34110
efilnukefesin <efil.threat@googlemail.com>
2019-04-30 12:36:01 +0200

    added some comments

--- src/PermissionServer/Controllers/PermissionsController.cs
+++ src/PermissionServer/Controllers/PermissionsController.cs
@@ -33,32 +33,12 @@ namespace PermissionServer.Controllers
 
         #region Methods
 
-        //#region Get
-        ////https://localhost:6000/api/permissions/
-        //[HttpGet]
-        //public ActionResult<SimpleResult<User>> Get()
-        //{
-        //    SimpleResult<User> result = new SimpleResult<User>("Nothing happenend");
-        //    IHeaderDictionary requestHeaders = Request.Headers;
-        //    if (requestHeaders.ContainsKey("Authorization"))
-        //    {
-        //        //TODO: do Token validation
-        //        //TODO: extract Subject Id from token and pass to permissionService
-        //        string subjectId = "88421113";
-        //        User user = permissionService.GetUser(subjectId);
-        //        //result = new SimpleResult<User>(user);  //TODO: find stackoverflow exception, caused by probably wrongly-typed Result
-        //        result = new SimpleResult<User>(new User("TestBob"));  //TODO: find stackoverflow exception, caused by probably wrongly-typed Result
-        //    }
-        //    else
-        //    {
-        //        result = new SimpleResult<User>("User not identified!");
-        //    }
-        //    return result;
-        //}
-        //#endregion Get
-
         #region Get
         //https://localhost:6000/api/permissions/
+        /// <summary>
+        /// returns a User which is associated with the current sub claim of the JWT token        
+        /// /// </summary>
+        /// <returns>a SimpleResult either having the User object as Payload or a descriptive error message.</returns>
         [HttpGet]
         [Authorize(Policy = "Bearer")]
         public ActionResult<SimpleResult<User>> Get()
