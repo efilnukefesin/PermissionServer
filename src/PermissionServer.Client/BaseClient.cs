44f8a41ec7a8ea05c18cf7d21ee57ed876c0ff2a
efilnukefesin <efil.threat@googlemail.com>
2019-05-04 22:24:38 +0200

    started structuring Clients in SDKs

---
+++ src/PermissionServer.Client/BaseClient.cs
@@ -0,0 +1,46 @@
+﻿using NET.efilnukefesin.Implementations.Base;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace PermissionServer.Client
+{
+    public abstract class BaseClient : BaseObject
+    {
+        #region Properties
+
+        protected HttpClient httpClient;
+
+        #endregion Properties
+
+        #region Construction
+
+        public BaseClient(Uri BaseUrl)
+        {
+            this.httpClient = new HttpClient();
+            this.httpClient.BaseAddress = BaseUrl;
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #region AddAuthenticationHeader
+        public void AddAuthenticationHeader(string value, string type = "Bearer")
+        {
+            this.httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(type, value);
+        }
+        #endregion AddAuthenticationHeader
+
+        #region dispose
+        protected override void dispose()
+        {
+            this.httpClient = null;
+        }
+        #endregion dispose
+
+        #endregion Methods
+    }
+}
---
+++ src/PermissionServer.SDK/Client.cs
@@ -0,0 +1,37 @@
+﻿using PermissionServer.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermissionServer.SDK
+{
+    public class Client : BaseClient
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        public Client(Uri BaseUrl) : base(BaseUrl)
+        {
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #region dispose
+        protected override void dispose()
+        {
+            //TODO: implement
+        }
+        #endregion dispose
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
