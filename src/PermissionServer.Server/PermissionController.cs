e9746ee1765ab0dcf7ba2baf20d8fe10c2837915
efilnukefesin <efil.threat@googlemail.com>
2019-05-04 21:54:56 +0200

    added not so secret plans (tm)

--- src/PermissionServer.Server/PermissionController.cs
+++ src/PermissionServer.Server/PermissionController.cs
@@ -7,5 +7,18 @@ namespace PermissionServer.Server
 {
     public abstract class PermissionController : ControllerBase
     {
+        //TODO: add helper to consolidate permission overviews
+        //TODO: declare common end points for common tasks
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
     }
 }
