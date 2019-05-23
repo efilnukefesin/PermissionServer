25814d043d29e2db45a0b329ad76f4db7ad2bf9e
efilnukefesin <efil.threat@googlemail.com>
2019-05-23 15:27:35 +0200

    started using commands ;-)

--- src/AdminApp/ViewModels/LoginViewModel.cs
+++ src/AdminApp/ViewModels/LoginViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using NET.efilnukefesin.Extensions.Wpf.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Security;
 using System.Text;
+using System.Windows.Input;
 
 namespace AdminApp.ViewModels
 {
@@ -15,6 +17,8 @@ namespace AdminApp.ViewModels
         public string Password { get; set; }  //just for lookup reasons, delete in a productive app
         public string Username { get; set; }
 
+        public ICommand OkCommand { get; set; }
+
         #endregion Properties
 
         #region Construction
@@ -22,11 +26,36 @@ namespace AdminApp.ViewModels
         public LoginViewModel()
         {
             this.Hint = "Test123";
+            this.setupCommands();
         }
 
         #endregion Construction
 
         #region Methods
+
+        #region setupCommands
+        //TODO: move to abstract base class?
+        protected void setupCommands()
+        {
+            this.OkCommand = new RelayCommand(this.okCommandExecute, this.okCommandCanExecute);
+        }
+        #endregion setupCommands
+
+        #region okCommandCanExecute
+        private bool okCommandCanExecute()
+        {
+            bool result = false;
+            result = !string.IsNullOrWhiteSpace(this.Username) && this.SecurePassword?.Length > 0;
+            return result;
+        }
+        #endregion okCommandCanExecute
+
+        #region okCommandExecute
+        private void okCommandExecute()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion okCommandExecute
 
         #endregion Methods
 
