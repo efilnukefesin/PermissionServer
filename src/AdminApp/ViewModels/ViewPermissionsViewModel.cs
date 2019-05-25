7dd5a312e530f05e4f748ea0d518c69a0493998e
efilnukefesin <efil.threat@googlemail.com>
2019-05-25 23:46:01 +0200

    did the childing.

--- src/AdminApp/ViewModels/ViewPermissionsViewModel.cs
+++ src/AdminApp/ViewModels/ViewPermissionsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using NET.efilnukefesin.Implementations.Mvvm.Attributes;
+﻿using Interfaces;
+using NET.efilnukefesin.Implementations.Mvvm.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Text;
 namespace AdminApp.ViewModels
 {
     [Locator("ViewPermissionsViewModel")]
-    internal class ViewPermissionsViewModel
+    internal class ViewPermissionsViewModel : BaseViewModel
     {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        public ViewPermissionsViewModel(IMessageBroker MessageBroker, BaseViewModel Parent = null)
+            : base(MessageBroker, Parent)
+        {
+
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #region dispose
+        protected override void dispose()
+        {
+
+        }
+        #endregion dispose
+
+        #region receiveMessage
+        protected override bool receiveMessage(string Text)
+        {
+            return false;
+        }
+        #endregion receiveMessage
+
+        #endregion Methods
     }
 }
--- src/AdminApp/ViewModels/ViewRolesViewModel.cs
+++ src/AdminApp/ViewModels/ViewRolesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using NET.efilnukefesin.Implementations.Mvvm.Attributes;
+﻿using Interfaces;
+using NET.efilnukefesin.Implementations.Mvvm.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Text;
 namespace AdminApp.ViewModels
 {
     [Locator("ViewRolesViewModel")]
-    internal class ViewRolesViewModel
+    internal class ViewRolesViewModel : BaseViewModel
     {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        public ViewRolesViewModel(IMessageBroker MessageBroker, BaseViewModel Parent = null)
+            : base(MessageBroker, Parent)
+        {
+
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #region dispose
+        protected override void dispose()
+        {
+
+        }
+        #endregion dispose
+
+        #region receiveMessage
+        protected override bool receiveMessage(string Text)
+        {
+            return false;
+        }
+        #endregion receiveMessage
+
+        #endregion Methods
     }
 }
