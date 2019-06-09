243f687eaac99b64d5ee5096fdb5105983d51fec
efilnukefesin <efil.threat@googlemail.com>
2019-06-09 23:44:54 +0200

    started adding Permission functionality

---
+++ src/AdminApp/UserControls/AddPermissionUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminApp.UserControls
+{
+    /// <summary>
+    /// Interaktionslogik für AddPermissionUserControl.xaml
+    /// </summary>
+    public partial class AddPermissionUserControl : UserControl
+    {
+        public AddPermissionUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
