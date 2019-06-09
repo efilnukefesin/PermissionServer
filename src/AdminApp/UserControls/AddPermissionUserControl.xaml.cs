df89d7dc281b72b2e8669f193067bd457415b739
efilnukefesin <efil.threat@googlemail.com>
2019-06-09 23:53:57 +0200

    worked on UserControl

--- src/AdminApp/UserControls/AddPermissionUserControl.xaml.cs
+++ src/AdminApp/UserControls/AddPermissionUserControl.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NET.efilnukefesin.Contracts.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,11 +18,31 @@ namespace AdminApp.UserControls
     /// <summary>
     /// Interaktionslogik für AddPermissionUserControl.xaml
     /// </summary>
-    public partial class AddPermissionUserControl : UserControl
+    public partial class AddPermissionUserControl : UserControl, INotifyPropertyChanged
     {
+        #region Properties
+
+        public bool IsIdle { get; set; } = true;
+
+        #endregion Properties
+
+        #region Construction
+
         public AddPermissionUserControl()
         {
             InitializeComponent();
         }
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
     }
 }
