7364961b99366a3b401ef247f7ab751b4e632aad
efilnukefesin <efil.threat@googlemail.com>
2019-06-30 22:52:23 +0200

    added rump for generic Navigation Presenter

---
+++ src/WPF.Shared/Navigation/WpfNavigationPresenter.cs
@@ -0,0 +1,47 @@
+﻿using NET.efilnukefesin.Contracts.Mvvm;
+using NET.efilnukefesin.Implementations.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF.Shared.Navigation
+{
+    public class WpfNavigationPresenter : BaseObject, INavigationPresenter
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        #endregion Construction
+
+        #region Methods
+
+        public void Back()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Present(string ViewUri, object DataContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterPresenter(object Presenter)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
