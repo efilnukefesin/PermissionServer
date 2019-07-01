ed7aaf8fe20e972a3e9793cc1400270d507805c8
efilnukefesin <efil.threat@googlemail.com>
2019-07-01 22:28:36 +0200

    adjusted test

--- test/CommonTests/SerializationTests.cs
+++ test/CommonTests/SerializationTests.cs
@@ -31,9 +31,9 @@ namespace CommonTests
         {
             #region DateTimeFromString
             [DataTestMethod]
-            //[DataRow("2019-07-01T14:51:0009180616+02:00", true)]
+            [DataRow("2019-07-01T14:51:0009180616+02:00", false)]
             [DataRow("2019-07-01T14:51+02:00", true)]
-            [DataRow("2019-07-01T14:51.101+02:00", true)]
+            [DataRow("2019-07-01T14:51.101+02:00", false)]
             public void DateTimeFromString(string Input, bool IsSuccessExpected)
             {
                 DateTimeOffset result;
