1e4218fd57d624f1c8cf2af660c79eb6de07049d
efilnukefesin <efil.threat@googlemail.com>
2019-07-01 21:33:59 +0200

    copied over and added c'tor

--- src/WPF.Shared/Navigation/WpfNavigationPresenter.cs
+++ src/WPF.Shared/Navigation/WpfNavigationPresenter.cs
@@ -1,8 +1,12 @@
-﻿using NET.efilnukefesin.Contracts.Mvvm;
+﻿using NET.efilnukefesin.Contracts.Logger;
+using NET.efilnukefesin.Contracts.Mvvm;
+using NET.efilnukefesin.Helpers;
 using NET.efilnukefesin.Implementations.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WPF.Shared.Navigation
 {
@@ -10,38 +14,152 @@ namespace WPF.Shared.Navigation
     {
         #region Properties
 
+        private Frame presentationFrame;
+        private object currentDataContext = null;
+
+        private string bufferedViewUri = null;
+        private object bufferedDataContext = null;
+
+        private string packPrefix = "pack://application:,,,/AdminApp;component/Views/";
+        private string typePrefix = "AdminApp.Views.";
+
+        private const string windowSuffix = "Window.xaml";
+
+        private Window currentWindow = null;
+        private Page currentPage = null;
+
+        private ILogger logger;
+
         #endregion Properties
 
         #region Construction
+
+        public WpfNavigationPresenter(string PackPrefix, string TypePrefix, ILogger Logger) : base()
+        {
+            this.packPrefix = PackPrefix;
+            this.typePrefix = TypePrefix;
+            this.logger = Logger;
+            //TODO: add logging
+        }
 
         #endregion Construction
 
         #region Methods
 
+        #region Back
         public void Back()
         {
-            throw new NotImplementedException();
+            //If Modal Window is shown, close; if page view is shown,  go one back, forward to Presenter Implementation.
+            if (this.currentWindow != null)
+            {
+                this.currentWindow.Close();
+            }
+            else if (this.currentPage != null)
+            {
+                this.presentationFrame.GoBack();
+            }
         }
+        #endregion Back
 
+        #region Present
         public bool Present(string ViewUri, object DataContext)
         {
-            throw new NotImplementedException();
-        }
+            bool result = false;
+            try
+            {
+                this.currentDataContext = DataContext;
+                Uri viewPackUri = new Uri(this.packPrefix + ViewUri);
+                if (ViewUri.EndsWith(WpfNavigationPresenter.windowSuffix))
+                {
+                    //show as modal window
+                    string typeName = ViewUri.Replace(".xaml", "");
+                    Type windowType = Type.GetType(this.typePrefix + typeName);
+                    var window = TypeHelper.CreateInstance(windowType);
+                    if (window is Window)
+                    {
+                        this.currentWindow = (window as Window);
+                        this.currentPage = null;
+                        (window as Window).Owner = Application.Current.MainWindow;
+                        (window as Window).DataContext = this.currentDataContext;
+                        (window as Window).ShowDialog();
+                    }
+                }
+                else
+                {
+                    if (this.presentationFrame != null)
+                    {
+                        this.presentationFrame.Navigate(viewPackUri);
 
+                        if ((Page)this.presentationFrame.Content != null)
+                        {
+                            this.currentWindow = null;
+                            this.currentPage = (Page)this.presentationFrame.Content;
+                            ((Page)this.presentationFrame.Content).DataContext = null;
+                        }
+
+                        this.bufferedViewUri = null;
+                        this.bufferedDataContext = null;
+
+                        result = true;
+                    }
+                    else
+                    {
+                        this.bufferedViewUri = ViewUri;
+                        this.bufferedDataContext = DataContext;
+                        result = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+        #endregion Present
+
+        #region RegisterPresenter
         public void RegisterPresenter(object Presenter)
         {
-            throw new NotImplementedException();
+            if (Presenter is Frame)
+            {
+                this.presentationFrame = (Frame)Presenter;
+                this.presentationFrame.Navigated += this.presentationFrame_Navigated;
+                if (this.bufferedViewUri != null && this.bufferedDataContext != null)
+                {
+                    this.Present(this.bufferedViewUri, this.bufferedDataContext);
+                }
+            }
         }
+        #endregion RegisterPresenter
 
+        #region presentationFrame_Navigated: set the datacontext after navigating as the content is now different
+        /// <summary>
+        /// set the datacontext after navigating as the content is now different
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void presentationFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if ((Page)e.Content != null)
+            {
+                this.currentWindow = null;
+                this.currentPage = (Page)e.Content;
+                ((Page)e.Content).DataContext = this.currentDataContext;
+            }
+        }
+        #endregion presentationFrame_Navigated
+
+        #region dispose
         protected override void dispose()
         {
-            throw new NotImplementedException();
+            this.presentationFrame = null;
+            this.currentDataContext = null;
+            this.bufferedDataContext = null;
+            this.bufferedViewUri = null;
         }
+        #endregion dispose
 
         #endregion Methods
-
-        #region Events
-
-        #endregion Events
     }
 }
