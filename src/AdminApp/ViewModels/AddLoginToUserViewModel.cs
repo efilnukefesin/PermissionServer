145108943ab27a7778ab3038afe62f75ead269c3
efilnukefesin <efil.threat@googlemail.com>
2019-07-02 23:22:49 +0200

    updated packages

--- src/AdminApp/ViewModels/AddLoginToUserViewModel.cs
+++ src/AdminApp/ViewModels/AddLoginToUserViewModel.cs
@@ -21,7 +21,7 @@ namespace AdminApp.ViewModels
         #region Properties
 
         public User SelectedUser { get; set; }
-        private string originalUserSerialized;
+        //private string originalUserSerialized;
         public bool HasChanged { get; set; } = false;
         public ObservableCollection<UnknownLogin> UnknownLogins { get; set; }
         public ICommand LoadedCommand { get; set; }
@@ -79,7 +79,8 @@ namespace AdminApp.ViewModels
         private void cancelCommandExecute()
         {
             //rewind changes
-            this.SelectedUser = JsonConvert.DeserializeObject<User>(this.originalUserSerialized);
+            //this.SelectedUser = JsonConvert.DeserializeObject<User>(this.originalUserSerialized);
+            this.SelectedUser.Restore();
             //close window
             this.navigationService.Back();
         }
@@ -150,7 +151,7 @@ namespace AdminApp.ViewModels
         {
             //make backup of selected user
             this.SelectedUser.Save();
-            this.originalUserSerialized = JsonConvert.SerializeObject(this.SelectedUser);
+            //this.originalUserSerialized = JsonConvert.SerializeObject(this.SelectedUser);
             //load stuff
             IEnumerable<UnknownLogin> unknownLogins = await this.client.GetUnknownLoginsAsync();
             this.UnknownLogins = new ObservableCollection<UnknownLogin>(unknownLogins);
@@ -239,17 +240,19 @@ namespace AdminApp.ViewModels
         /// </summary>
         private void checkIfChanged()
         {
-            if (this.originalUserSerialized != null)
-            {
-                if (!this.originalUserSerialized.Equals(JsonConvert.SerializeObject(this.SelectedUser)))
-                {
-                    this.HasChanged = true;
-                }
-                else
-                {
-                    this.HasChanged = false;
-                }
-            }
+            this.HasChanged = this.SelectedUser.DiffersFromMemory();
+
+            //if (this.originalUserSerialized != null)
+            //{
+            //    if (!this.originalUserSerialized.Equals(JsonConvert.SerializeObject(this.SelectedUser)))
+            //    {
+            //        this.HasChanged = true;
+            //    }
+            //    else
+            //    {
+            //        this.HasChanged = false;
+            //    }
+            //}
         }
         #endregion checkIfChanged
 
