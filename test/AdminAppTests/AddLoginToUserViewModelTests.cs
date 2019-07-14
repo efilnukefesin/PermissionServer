ea758338bf3ec114407a0a9ea59f9c3df39bb9c6
efilnukefesin <efil.threat@googlemail.com>
2019-07-14 21:51:45 +0200

    straightened test method

--- test/AdminAppTests/AddLoginToUserViewModelTests.cs
+++ test/AdminAppTests/AddLoginToUserViewModelTests.cs
@@ -44,6 +44,16 @@ namespace AdminAppTests
         [TestClass]
         public class AddLoginToUserViewModelMethods : AddLoginToUserViewModelTests
         {
+            #region setupAddLoginToUserViewModel
+            private void setupAddLoginToUserViewModel(AddLoginToUserViewModel addLoginToUserViewModel, string SearchText)
+            {
+                addLoginToUserViewModel.UnknownLogins.Add(new PermissionServer.Models.UnknownLogin("123"));
+                addLoginToUserViewModel.UnknownLogins.Add(new PermissionServer.Models.UnknownLogin("345"));
+                addLoginToUserViewModel.Text = SearchText;
+                addLoginToUserViewModel.UpdateSearchResults();
+            }
+            #endregion setupAddLoginToUserViewModel
+
             #region ChangeText
             [TestMethod]
             public void ChangeText()
@@ -51,14 +61,24 @@ namespace AdminAppTests
                 DiSetup.Tests();
                 DiHelper.Register<INavigationPresenter, DummyNavigationPresenter>();
                 AddLoginToUserViewModel addLoginToUserViewModel = DiHelper.GetService<AddLoginToUserViewModel>();
-                addLoginToUserViewModel.UnknownLogins.Add(new PermissionServer.Models.UnknownLogin("123"));
-                addLoginToUserViewModel.UnknownLogins.Add(new PermissionServer.Models.UnknownLogin("345"));
-                addLoginToUserViewModel.Text = "23";
-                addLoginToUserViewModel.UpdateSearchResults();
+                this.setupAddLoginToUserViewModel(addLoginToUserViewModel, "234");
 
                 Assert.AreEqual("Add new Sub ID", addLoginToUserViewModel.ButtonText);
             }
             #endregion ChangeText
+
+            #region FindOne
+            [TestMethod]
+            public void FindOne()
+            {
+                DiSetup.Tests();
+                DiHelper.Register<INavigationPresenter, DummyNavigationPresenter>();
+                AddLoginToUserViewModel addLoginToUserViewModel = DiHelper.GetService<AddLoginToUserViewModel>();
+                this.setupAddLoginToUserViewModel(addLoginToUserViewModel, "23");
+
+                Assert.AreEqual("Add Selected Sub ID", addLoginToUserViewModel.ButtonText);
+            }
+            #endregion FindOne
         }
         #endregion AddLoginToUserViewModelMethods
     }
