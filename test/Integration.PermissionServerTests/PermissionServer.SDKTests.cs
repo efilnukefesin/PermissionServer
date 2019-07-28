14e20b526e612a65db3424f3bfdba82bcd26a14b
efilnukefesin <efil.threat@googlemail.com>
2019-07-28 21:41:37 +0200

    added an integration test

---
+++ test/Integration.PermissionServerTests/PermissionServer.SDKTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Integration.PermissionServerTests
+{
+	[TestClass]
+    public class PermissionServer
+    {
+    }
+}
